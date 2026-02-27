7cd9dea40c75bbc667da5aad57e0c29f2d56f3eb
CrowDx2468 <147876251+CrowDx2468@users.noreply.github.com>
2026-02-27 21:29:50 +0300

    Add files via upload

---
+++ sortLab.cpp
@@ -0,0 +1,140 @@
+﻿#include <iomanip>
+#include <iostream>
+#include <ctime>
+#include <stdlib.h>
+
+using namespace std;
+int arr1[1000] = {}, arr2[10000] = {}, arr3[100000] = {}, arr4[1000000] = {};
+int main()
+{
+	srand(time(NULL));
+	setlocale(LC_ALL, "");
+	int randomNumber = 1 + rand() % 1000;
+	int size1 = 1000, size2 = 10000, size3 = 100000, size4 = 1000000;
+	int tmp, i, j, pos;
+	
+	tm newTime;
+	time_t now = time(0);
+	localtime_s(&newTime, &now);
+
+	for (int i = 0; i < 1000; i++) {
+		arr1[i] = randomNumber;
+		randomNumber = 1 + rand() % 1000;
+	}
+
+	now = time(0);
+	localtime_s(&newTime, &now);
+	cout << newTime.tm_hour << ":" << newTime.tm_min << ":" << newTime.tm_sec << " до сортировки первого массива" << endl;
+
+	for (i = 0; i < size1; i++) 
+	{
+		pos = i;
+		tmp = arr1[i];
+		for (j = i + 1; j < size1; ++j) 
+		{
+			if (arr1[j] < tmp)
+			{
+				pos = j;
+				tmp = arr1[j];
+			}
+		}
+		arr1[pos] = arr1[i];
+		arr1[i] = tmp;
+	}
+	
+	now = time(0);
+	localtime_s(&newTime, &now);
+	cout << newTime.tm_hour << ":" << newTime.tm_min << ":" << newTime.tm_sec << " после сортировки первого массива" << endl;
+
+	for (int i = 0; i < 10000; i++) {
+		arr2[i] = randomNumber;
+		randomNumber = 1 + rand() % 1000;
+		
+	}
+
+	now = time(0);
+	localtime_s(&newTime, &now);
+	cout << newTime.tm_hour << ":" << newTime.tm_min << ":" << newTime.tm_sec << " до сортировки второго массива" << endl;
+
+	for (i = 0; i < size2; i++)
+	{
+		pos = i;
+		tmp = arr2[i];
+		for (j = i + 1; j < size2; ++j)
+		{
+			if (arr2[j] < tmp)
+			{
+				pos = j;
+				tmp = arr2[j];
+			}
+		}
+		arr2[pos] = arr2[i];
+		arr2[i] = tmp;
+	}
+
+	now = time(0);
+	localtime_s(&newTime, &now);
+	cout << newTime.tm_hour << ":" << newTime.tm_min << ":" << newTime.tm_sec << " после сортировки второго массива" << endl;
+
+
+	for (int i = 0; i < 100000; i++) {
+		arr3[i] = randomNumber;
+		randomNumber = 1 + rand() % 1000;
+	}
+
+	now = time(0);
+	localtime_s(&newTime, &now);
+	cout << newTime.tm_hour << ":" << newTime.tm_min << ":" << newTime.tm_sec << " до сортировки третьего массива" << endl;
+
+	for (i = 0; i < size3; i++)
+	{
+		pos = i;
+		tmp = arr3[i];
+		for (j = i + 1; j < size3; ++j)
+		{
+			if (arr3[j] < tmp)
+			{
+				pos = j;
+				tmp = arr3[j];
+			}
+		}
+		arr3[pos] = arr3[i];
+		arr3[i] = tmp;
+	}
+
+	now = time(0);
+	localtime_s(&newTime, &now);
+	cout << newTime.tm_hour << ":" << newTime.tm_min << ":" << newTime.tm_sec << " после сортировки третьего массива" << endl;
+
+	for (int i = 0; i < 1000000; i++) {
+		arr4[i] = randomNumber;
+		randomNumber = 1 + rand() % 1000;
+		
+	}
+
+	now = time(0);
+	localtime_s(&newTime, &now);
+	cout << newTime.tm_hour << ":" << newTime.tm_min << ":" << newTime.tm_sec << " до сортировки четвёртого массива" << endl;
+
+	for (i = 0; i < size4; i++)
+	{
+		pos = i;
+		tmp = arr4[i];
+		for (j = i + 1; j < size4; ++j)
+		{
+			if (arr4[j] < tmp)
+			{
+				pos = j;
+				tmp = arr4[j];
+			}
+		}
+		arr4[pos] = arr4[i];
+		arr4[i] = tmp;
+	}
+
+	now = time(0);
+	localtime_s(&newTime, &now);
+	cout << newTime.tm_hour << ":" << newTime.tm_min << ":" << newTime.tm_sec << " после сортировки четвёртого массива" << endl;
+}
+
+
---
+++ sortLabBubble.cpp
@@ -0,0 +1,119 @@
+﻿#include <iomanip>
+#include <iostream>
+#include <ctime>
+#include <stdlib.h>
+
+using namespace std;
+int arr1[1000] = {}, arr2[10000] = {}, arr3[100000] = {}, arr4[1000000] = {};
+int main()
+{
+	srand(time(NULL));
+	setlocale(LC_ALL, "");
+	int randomNumber = 1 + rand() % 1000;
+	int size1 = 1000, size2 = 10000, size3 = 100000, size4 = 1000000;
+	int tmp, i, j;
+
+	tm newTime;
+	time_t now = time(0);
+	localtime_s(&newTime, &now);
+
+	for (int i = 0; i < 1000; i++) {
+		arr1[i] = randomNumber;
+		randomNumber = 1 + rand() % 1000;
+	}
+
+	now = time(0);
+	localtime_s(&newTime, &now);
+	cout << newTime.tm_hour << ":" << newTime.tm_min << ":" << newTime.tm_sec << " до сортировки первого массива" << endl;
+	for (i = 0; i < size1 - 1; ++i) 
+	{
+		for (j = 0; j < size1 - 1; ++j)
+		{
+			if (arr1[j + 1] < arr1[j])
+			{
+				tmp = arr1[j + 1];
+				arr1[j + 1] = arr1[j];
+				arr1[j] = tmp;
+			}
+		}
+	}
+	now = time(0);
+	localtime_s(&newTime, &now);
+	cout << newTime.tm_hour << ":" << newTime.tm_min << ":" << newTime.tm_sec << " после сортировки первого массива" << endl;
+
+	for (int i = 0; i < 10000; i++) {
+		arr2[i] = randomNumber;
+		randomNumber = 1 + rand() % 1000;
+	}
+
+	now = time(0);
+	localtime_s(&newTime, &now);
+	cout << newTime.tm_hour << ":" << newTime.tm_min << ":" << newTime.tm_sec << " до сортировки второго массива" << endl;
+
+	for (i = 0; i < size2 - 1; ++i)
+	{
+		for (j = 0; j < size2 - 1; ++j)
+		{
+			if (arr2[j + 1] < arr2[j])
+			{
+				tmp = arr2[j + 1];
+				arr2[j + 1] = arr2[j];
+				arr2[j] = tmp;
+			}
+		}
+	}
+	now = time(0);
+	localtime_s(&newTime, &now);
+	cout << newTime.tm_hour << ":" << newTime.tm_min << ":" << newTime.tm_sec << " после сортировки второго массива" << endl;
+
+	for (int i = 0; i < 100000; i++) {
+		arr3[i] = randomNumber;
+		randomNumber = 1 + rand() % 1000;
+	}
+
+	now = time(0);
+	localtime_s(&newTime, &now);
+	cout << newTime.tm_hour << ":" << newTime.tm_min << ":" << newTime.tm_sec << " до сортировки третьего массива" << endl;
+
+	for (i = 0; i < size3 - 1; ++i)
+	{
+		for (j = 0; j < size3 - 1; ++j)
+		{
+			if (arr3[j + 1] < arr3[j])
+			{
+				tmp = arr3[j + 1];
+				arr3[j + 1] = arr3[j];
+				arr3[j] = tmp;
+			}
+		}
+	}
+	now = time(0);
+	localtime_s(&newTime, &now);
+	cout << newTime.tm_hour << ":" << newTime.tm_min << ":" << newTime.tm_sec << " после сортировки третьего массива" << endl;
+
+	for (int i = 0; i < 1000000; i++) {
+		arr4[i] = randomNumber;
+		randomNumber = 1 + rand() % 1000;
+	}
+
+	now = time(0);
+	localtime_s(&newTime, &now);
+	cout << newTime.tm_hour << ":" << newTime.tm_min << ":" << newTime.tm_sec << " до сортировки четвёртого массива" << endl;
+
+	for (i = 0; i < size4 - 1; ++i)
+	{
+		for (j = 0; j < size4 - 1; ++j)
+		{
+			if (arr4[j + 1] < arr4[j])
+			{
+				tmp = arr4[j + 1];
+				arr4[j + 1] = arr4[j];
+				arr4[j] = tmp;
+			}
+		}
+	}
+	now = time(0);
+	localtime_s(&newTime, &now);
+	cout << newTime.tm_hour << ":" << newTime.tm_min << ":" << newTime.tm_sec << " после сортировки четвёртого массива" << endl;
+
+}
